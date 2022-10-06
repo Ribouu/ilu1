2cb0c7c488a2fda0850d93cfdc73fcac2d5471c9
Lucas Récan <76816773+Ribouu@users.noreply.github.com>
2022-10-06 14:11:48 +0200

    Première partie du village Gaulois - jusqu'à 'afficherVillageois'

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,16 +1,58 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
+	
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	public Gaulois trouverHabitant(int numVillageois) {
+		return villageois[numVillageois];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+		for (int i=0; i<nbVillageois; i++) {
+			System.out.println("- " + trouverHabitant(i).getNom());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		// Gaulois gaulois = village.trouverHabitant(30);
+		// Nous avons une exception car les adresses du tableau villageois vont de 0 à 29, pas 30.
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+		Gaulois obelix = new Gaulois("Obélix", 20);
+		village.ajouterHabitant(obelix);
+		// Gaulois gaulois = village.trouverHabitant(1);
+		// System.out.println(gaulois);
+		// Les lignes ci-dessus donne le résultat null car Astérix est en adresse 0, et il n'y a pas de
+		// villageois en adresse 1.
+		village.afficherVillageois();
+	}
 }
+
+
