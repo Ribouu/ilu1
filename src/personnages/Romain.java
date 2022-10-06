fa622fd6e150dca48c57788ca7b99a8be989f226
Lucas Récan <76816773+Ribouu@users.noreply.github.com>
2022-10-06 14:53:09 +0200

    Énumérés + fin du TP

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,13 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -41,12 +44,41 @@ public class Romain {
 		return forceToujoursPositive();
 	}
 	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 0 :
+			equipements[0] = equipement;
+			System.out.println("Le soldat " + getNom() + " s'équipe d'un " + equipement + " !");
+			nbEquipement += 1;
+			break;
+		case 1 :
+			if  (equipements[0]==equipement) {
+				System.out.println("Le soldat " + getNom() + " possède déjà un " + equipement + " !");
+			} else {
+				equipements[1] = equipement;
+				System.out.println("Le soldat " + getNom() + " s'équipe d'un " + equipement + " !");
+				nbEquipement += 1;
+			}
+			break;
+		default:
+			System.out.println("Le soldat " + getNom() + " est déjà bien protégé !");
+		}
+	}
 	public static void main(String[] args) {
 		Romain cesar = new Romain("César", 6);
 		assert cesar.isInvariantSatified();
 		System.out.println(cesar.prendreParole());
 		cesar.parler("Bonjour mon peup... Astérix non !!");
 		cesar.recevoirCoup(4);
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+		System.out.println(casque);
+		System.out.println(bouclier);
+		cesar.sEquiper(casque);
+		cesar.sEquiper(casque);
+		cesar.sEquiper(bouclier);
+		cesar.sEquiper(casque);
+		
 	}
 	
 }
